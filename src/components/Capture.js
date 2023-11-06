eb480ea0cfdcc229526e61dd49a1d14a711c00eb
Mei air <mei@astra.cloud>
2023-11-06 12:22:41 +0800

    Use canvas for show detect rect

--- src/components/Capture.js
+++ src/components/Capture.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import Button from '@material-ui/core/Button';
 import Webcam from 'react-webcam'
 import { useTheme } from '@material-ui/core/styles'
@@ -8,14 +8,25 @@ import { IconButton } from '@material-ui/core';
 import CameraIcon from '@material-ui/icons/Camera';
 import FlipCameraIosIcon from '@material-ui/icons/FlipCameraIos';
 import CircularProgress from '@material-ui/core/CircularProgress';
-
+import Backdrop from '@material-ui/core/Backdrop';
+import { makeStyles } from '@material-ui/core/styles';
+import classNames from 'classnames';
 import 'custom.scss';
+
+const useStyles = makeStyles((theme) => ({
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: '#fff',
+  },
+}));
+
 // import { clear } from '@testing-library/user-event/dist/clear';
 const WebcamComponent = () => <Webcam />
 const size = 1280;
 function dataURItoBlob(dataURI) {
   // convert base64/URLEncoded data component to raw binary data held in a string
   var byteString;
+  if ( ! dataURI ) return;
   if (dataURI.split(',')[0].indexOf('base64') >= 0)
     byteString = atob(dataURI.split(',')[1]);
   else
@@ -33,25 +44,43 @@ function dataURItoBlob(dataURI) {
   return new Blob([ia], { type: mimeString });
 }
 
+function drawlines(canvas, context, item){
+
+  if (!item) return;
+  const { x, y, width, height } = item.box;
+  context.beginPath();
+  context.moveTo(x, y);
+  context.lineTo(x + width, y);
+  context.lineTo(x + width, y + height);
+  context.lineTo(x, y + height);
+  context.lineTo(x, y);
+  context.lineWidth = 5;
+  // line color
+  context.strokeStyle = "white";
+  context.stroke();
+}
+
 const Capture = () => {
+  const classes = useStyles();
   const webcamRef = React.useRef(null)
   const imgRef = React.useRef(null)
   const theme = useTheme();
-  const matches = useMediaQuery(theme.breakpoints.up('lg'));
+  const matches = useMediaQuery(theme.breakpoints.up('md'));
+  console.log(matches, ':matches');
   // const { Canvas, Image, ImageData } = canvas
 
   // console.log(faceapi.nets, ':nets')
   const [picture, setPicture] = useState('')
   const [faceLen, setFaceLen] = useState(null)
   const [faceDetect, setFaceDetect] = useState(true)
-  const [ facingMode, setFacingMode ] = useState('user')
-  const [ facePicture, setFacePicture ] = useState([])
-  const [ timer, setTimer ] = useState(null)
+  const [facingMode, setFacingMode] = useState('environment')
+  const [facePicture, setFacePicture] = useState([])
+  const [timer, setTimer] = useState(null)
   const doTimer = () => {
-      if (timer) {
-        clearTimeout(timer);
-        setTimer(null);
-      }
+    if (timer) {
+      clearTimeout(timer);
+      setTimer(null);
+    }
     const videoTimout = setTimeout(() => {
       if (webcamRef.current) {
         setFaceDetect(false);
@@ -64,64 +93,114 @@ const Capture = () => {
       doTimer();
     }, 500);
     setTimer(videoTimout)
-}
+  }
   const videoConstraints = {
     width: size,
+    height: matches ? size * 9 / 16 : size,
     aspectRatio: matches ? 16 / 9 : 1 / 1,
     facingMode: facingMode,
   }
   console.log(matches, ':matches md');
 
-const Crop = (input, index) => {
+  const Crop = useCallback((rows) => {
+  if (!rows || rows.length < 1) return;
+  let imgDatasets = [];
+    setFaceDetect(true);
+    rows.map((item, index) => {
+      console.log(item, ':[crop]item waiting');
+      const { x, y, width, height } = item?.box;
+      const elem = document.createElement('canvas');
+      elem.width = width;
+      elem.height = height;
+      const ctx = elem.getContext('2d');
+      // img.width and img.height will contain the original dimensions
+      ctx.drawImage(picture, x, y, width, height, 0, 0, width, height);
+      ctx.canvas.toBlob((blob) => {
+        const file = new File([blob], `face_${index}`, {
+          type: 'image/jpeg',
+          lastModified: Date.now()
+        });
+        console.log(file, ':file');
+        const objectURL = URL.createObjectURL(file);
+        console.log(objectURL, ':[Crop]objectURL');
+        const imgItem = {
+          width: width,
+          height: height,
+          src: objectURL,
+        }
+        imgDatasets.push(imgItem)
+        if (imgDatasets.length === rows.length) {
+          setFacePicture(imgDatasets);
+          setFaceDetect(false);
+          faceapi.draw.drawDetections(document.querySelector('#showDetection'), rows);
+        }
+      }, 'image/jpeg', 1);
+    });
+    // console.log(input, ':[Crop]input');
+  }, [faceLen, picture])
+
+  /*
+  const Crop = (input, index, callBack) => {
     console.log(input, ':[Crop]input');
+    console.log(index, ':[Crop]index');
     const elem = document.createElement('canvas');
     elem.width = input.width;
     elem.height = input.height;
     const ctx = elem.getContext('2d');
     // img.width and img.height will contain the original dimensions
     ctx.drawImage(document.querySelector('#shot'), input.x, input.y, input.width, input.height, 0, 0, input.width, input.height);
-    ctx.canvas.toBlob((blob) => {
-        const file = new File([blob], `face_${index}`, {
-            type: 'image/jpeg',
-            lastModified: Date.now()
-        });
-        console.log(file, ':file');
-        const objectURL = URL.createObjectURL(file);
-        console.log(objectURL, ':[Crop]objectURL');
-    const f = facePicture.concat(objectURL);
-    setFacePicture(f);
+    const rs = ctx.canvas.toBlob((blob) => {
+      const file = new File([blob], `face_${index}`, {
+        type: 'image/jpeg',
+        lastModified: Date.now()
+      });
+      console.log(file, ':file');
+      const objectURL = URL.createObjectURL(file);
+      imgDatasets.push(objectURL)
+      console.log(objectURL, ':[Crop]objectURL');
+      // console.log(facePicture, ':[Crop]facePicture prev');
     }, 'image/jpeg', 1);
   }
+  */
   const DetectFace = async () => {
-    setFaceDetect(true);
-    const results = await faceapi.detectAllFaces(document.querySelector('#shot'))
+    const results = await faceapi.detectAllFaces(picture);
     // const canvases = await faceapi.extractFaces([document.querySelector('#shot')])
     // console.log(canvases, ':canvases');
     setFaceLen(results.length);
     // const {x, y, width, height} = results[0];
     // console.log(x, y, width, height, ':[detectFace]x, y, width, height');
-    setFaceDetect(false);
-    console.log(results[0].box, ':[detectFace] results');
-    results.map((item, index) => {
-      console.log(item, ':item');
-      const {x, y, width, height} = item.box;
-      Crop({x, y, width, height}, index);
-    });
-
+    // setFaceDetect(false);
+    console.log(results, ':[detectFace] results');
+    if (results.length > 0) {
+      Crop(results);
+    } else {
+      setFaceDetect(false);
+    }
   }
   const DoCapture = React.useCallback(() => {
+    console.log(matches, ':[DoCapture]matches');
     const pictureSrc = matches ? webcamRef.current.getScreenshot() : webcamRef.current.getScreenshot({ width: size, height: size })
     console.log(pictureSrc, ':pictureSrc');
     dataURItoBlob(pictureSrc);
     const objectURL = URL.createObjectURL(dataURItoBlob(pictureSrc));
-    setPicture(objectURL);
-    setTimeout(() => {
-      DetectFace()
-    }, 500)
-  })
+    const shotImg = new Image();
+    setPicture(shotImg);
+    shotImg.src = objectURL;
+    shotImg.onload = () => {
+      // alert(shotImg.width);
+      var c = document.getElementById("showDetection");
+      c.width = shotImg.width;
+      c.height = shotImg.height;
+      var ctx = c.getContext("2d");
+      ctx.drawImage(shotImg, 0, 0);
+    }
+    setFaceDetect(true);
+  }, [webcamRef, matches, picture])
 
   const loadModels = async () => {
-    await faceapi.nets.ssdMobilenetv1.loadFromUri(`/models`)
+    const path = "https://justadudewhohacks.github.io/face-api.js/models/"
+    // await faceapi.loadSsdMobilenetv1Model('/models')
+    await faceapi.nets.ssdMobilenetv1.loadFromUri(path)
     // await faceapi.nets.faceLandmark68Net.loadFromUri('/models')
     // await faceapi.nets.faceRecognitionNet.loadFromUri('/models')
   }
@@ -138,6 +217,16 @@ const Crop = (input, index) => {
     // console.log(rs, ':rs');
   }, [])
 
+  useEffect(() => {
+    console.log(facePicture, ':facePicture');
+  }, [facePicture]);
+
+  useEffect(() => {
+    console.log(picture, ':picture');
+    if (!picture) return;
+    DetectFace()
+  }, [picture])
+
   return (
     <>
       <header>
@@ -152,30 +241,38 @@ const Crop = (input, index) => {
 
           <div class="row justify-content-center">
             <div class="col-md-6 col-12 position-relative">
-              { faceDetect ? <div className="position-absolute w-100 h-100 d-flex justify-content-center align-items-center"><CircularProgress /></div> : null }
-              {picture == '' ? (
+              <div className={ classNames("ratio ratio-1x1", { "ratio-16x9" : matches }) }>
+              <Backdrop className={classes.backdrop} open={faceDetect}>
+        <CircularProgress color="inherit" />
+      </Backdrop>
                 <Webcam
+                  style={{ marginTop: picture ? '-1000em' : '0'}}
                   audio={false}
                   width={'100%'}
                   height={'100%'}
                   imageSmoothing={false}
                   ref={webcamRef}
                   screenshotFormat="image/jpeg"
-                  videoConstraints={Object.assign(videoConstraints, {facingMode })}
+                  videoConstraints={Object.assign(videoConstraints, { facingMode })}
                 />
-              ) : (
+              {picture && (
+                <div className="d-flex flex-column">
+                {/*
                 <img id="shot" src={picture} width={'100%'} ref={imgRef} />
+              */}
+                <canvas id="showDetection"></canvas>
+                </div>
               )}
+              </div>
             </div>
           </div>
-        </div>
         <div class="d-flex justify-content-center align-items-center">
           {picture != '' ? (
             <Button
               disabled={faceDetect}
               variant="contained"
+              className='mt-3'
               color="primary"
-              endIcon={<CameraIcon />}
               onClick={(e) => {
                 e.preventDefault();
                 setPicture('');
@@ -199,6 +296,7 @@ const Crop = (input, index) => {
               Capture
             </Button>
           )}
+          { ! picture &&
           <IconButton
             disabled={faceDetect}
             color="primary"
@@ -207,20 +305,22 @@ const Crop = (input, index) => {
               setFacingMode(facingMode === 'user' ? 'environment' : 'user')
             }}
           >
-          <FlipCameraIosIcon fontSize="large"/>
+            <FlipCameraIosIcon fontSize="large" />
           </IconButton>
+          }
         </div>
         <div className='d-flex justify-content-center'>
           {faceLen !== null ? <span>Face Detected: {faceLen}</span> : null}
         </div>
-        <div>
-        { facePicture?.length > 0 && facePicture.map((item, index) => {
-          return (
-            <div className='d-flex justify-content-center'>
-              <img src={item} width={matches ? 200 : 100} />
-            </div>
-          )
-        })}
+        <div className='d-flex justify-content-center p-4 flex-wrap'>
+          {facePicture?.length > 0 && facePicture.map((item, index) => {
+            return (
+              <div id={`${index}_img`} key={`${index}_img`} className='d-flex justify-content-center'>
+                <img src={item.src} width={ item.width/3 } height={item.height/3} />
+              </div>
+            )
+          })}
+        </div>
         </div>
       </main>
     </>
